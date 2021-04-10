495d919f87219a3369238402e132b3ff8954a688
GongYuQi <846589271@qq.com>
2021-04-10 19:00:51 +0800

    更新

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,6 @@
 $(function () {
+    // const baseUrl = 'http://ajax.frontend.itheima.net';
+    const baseUrl = 'http://api-breakingnews-web.itheima.net';
     $('#link_reg').on('click', function () {
         $('.login_box').hide();
         $('.reg_box').show();
@@ -7,5 +9,10 @@ $(function () {
     $('#link_login').on('click', function () {
         $('.reg_box').hide();
         $('.login_box').show();
+    });
+
+    const { form } = layui;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码应在6-12位之间!']
     });
 })
