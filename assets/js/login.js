effbbb5a5134d68b7a20afa6711e346f9c3c7690
GongYuQi <846589271@qq.com>
2021-04-10 19:15:54 +0800

    更新

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,8 +11,15 @@ $(function () {
         $('.login_box').show();
     });
 
+    // 结构赋值 将layui对象中的form抽离出来
     const { form } = layui;
     form.verify({
-        pwd: [/^[\S]{6,12}$/, '密码应在6-12位之间!']
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格!'],
+        repwd: function (value) {
+            const pwd = $('.reg_box [name=password]').val();
+            if (value !== pwd) {
+                return '两次输入的密码不一致！';
+            }
+        }
     });
 })
