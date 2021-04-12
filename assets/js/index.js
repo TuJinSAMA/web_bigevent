2368dd773ad666adfa34975f8f26b9672aa5f29b
GongYuQi <846589271@qq.com>
2021-04-12 11:09:16 +0800

    更新

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,32 @@
 $(function () {
+    const token = localStorage.getItem('token');
+    initPage();
 
+
+
+
+
+    function initPage() {
+        //获取用户基本信息
+        $.ajax({
+            type: "GET",
+            url: '/my/userinfo',
+            headers: {
+                Authorization: token
+            },
+            success: res => {
+                initUserinfo(res.data);
+            }
+        });
+    }
+    function initUserinfo(data) {
+        if (data.user_pic) {
+            $('.text-avatar').hide();
+            $('.portrait').attr('src', data.user_pic);
+        } else {
+            $('.portrait').hide();
+
+        }
+        $('#welcome').html(`欢迎 ${data.username}`);
+    }
 });
