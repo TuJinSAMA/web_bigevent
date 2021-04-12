aebfe3bc9fc928d33e46176eb4cee7592193178b
GongYuQi <846589271@qq.com>
2021-04-12 16:49:07 +0800

    创建修改密码页面

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -2,17 +2,20 @@ $(function () {
     layui.form.verify({
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         samePwd: function (value) {
-            const oldpwd = $('.changeInfo [name=oldPwd]').val();
-            if (value === oldpwd.val()) {
+            const oldpwd = $('.changePwd [name=oldpwd]').val();
+            if (value === oldpwd) {
                 return '新旧密码不能相同！'
             }
         },
         repwd: function (value) {
-            const newpwd = $('.changeInfo [name=newpwd]').val();
+            const newpwd = $('.changePwd [name=newpwd]').val();
             if (value !== newpwd) {
                 return '两次输入的密码不一致！';
             }
         }
     });
+    $('.changePwd').on('submit', function (e) {
+        e.preventDefault();
 
+    });
 });
