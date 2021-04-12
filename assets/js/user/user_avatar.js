0012b9c13847b300a714bdeca22445d2c4538b73
GongYuQi <846589271@qq.com>
2021-04-12 21:29:20 +0800

    创建了更换头像页面

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -10,4 +10,18 @@ $(function () {
     $('.uploadImg').on('click', function () {
         $('#chooseImg').click();
     });
+    $('#chooseImg').on('change', function (e) {
+        if (e.target.files.length <= 0) return layui.layer.msg('请选择图片!');
+        const imgObj = e.target.files[0];
+        const imgUrl = URL.createObjectURL(imgObj);
+        console.log(imgUrl);
+        // 'destroy' 销毁当前裁剪区域
+        //  attr('src',imgUrl) 将image的src替换为选择的图片
+        //  再次应用设置项
+        $image.cropper('destroy').attr('src', imgUrl).cropper(options);
+    })
+    $('.changeBtn').on('click', function () {
+        console.log(1);
+    });
+
 })
